9122621805a5ba1e4f22da1f902e86035c2ab2c8
taoteg <taoteg@gmail.com>
2017-10-05 16:35:14 -0500

    Established RESTful API for users. Created dynmic routers to respond to api requests.

--- config/settings/base.py
+++ config/settings/base.py
@@ -63,9 +63,11 @@ DJANGO_APPS = (
 )
 
 THIRD_PARTY_APPS = (
+    'rest_framework',
 )
 
 LOCAL_APPS = (
+    'project.api',
 )
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
@@ -165,3 +167,9 @@ STATICFILES_FINDERS = (
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = str(APPS_DIR('media'))
+
+
+# REST Framework
+
+REST_FRAMEWORK = {
+}
